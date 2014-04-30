fd2eed3cbd386731140b00f522042cf83d354d85
Elisha Cook <elisha@elishacook.com>
2014-04-30 15:09:09 -0400

    Only consider posts for most recent blog post

--- flaneur/jobs/last_blog_post.py
+++ flaneur/jobs/last_blog_post.py
@@ -16,7 +16,7 @@ def update(options):
                    passwd=options['mysql_passwd'],
                    db=options['mysql_db'])
     cur = conn.cursor()
-    cur.execute("SELECT post_date_gmt FROM wp_posts WHERE post_status='publish' ORDER BY post_date_gmt DESC LIMIT 1")
+    cur.execute("SELECT post_date_gmt FROM wp_posts WHERE post_type='post' AND post_status='publish' AND post_date_gmt < NOW() ORDER BY post_date_gmt DESC LIMIT 1")
     last_post_date = cur.fetchone()[0]
     now = datetime.utcnow()
     data['count'] = (now - last_post_date).days
